087d5509dfb1eaf29adf51e7870b04cd76392b2b
CrazyIvanftw <gbaustin89@gmail.com>
2017-02-06 10:59:51 +0100

    Exercises

---
+++ Ex1/src/ex1/Assignment.java
@@ -0,0 +1,8 @@
+package ex1;
+
+public class Assignment implements Statement {
+	
+	private String var;
+	private Expression rightSide;
+
+}
---
+++ Ex1/src/ex1/BlockStatement.java
@@ -0,0 +1,9 @@
+package ex1;
+
+import java.util.List;
+
+public class BlockStatement implements Statement {
+	
+	private List<Statement> block;
+
+}
